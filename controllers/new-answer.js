ec8dd3ea908462ceb95bccf9fc8caa0192292ae5
jocelyn <jfrodsla@gmail.com>
2015-06-12 10:26:43 -0700

    add basic functionality to new-answer; adjusts styling on forms

---
+++ controllers/new-answer.js
@@ -0,0 +1,23 @@
+Quiz.NewAnswerController = Ember.Controller.extend({
+  needs: ['question'],
+  actions: {
+    delete: function(){
+      if(confirm("are you sure?")) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('questions');
+      }
+    },
+    save: function() {
+      var answer = this.store.createRecord('answer', {
+        text: this.get('text')
+      });
+      answer.save();
+
+      var question = this.get('model');
+      question.get('answers').pushObject(answer);
+      question.save();
+
+      this.transitionToRoute('question', question.id);
+    }
+  }
+});
